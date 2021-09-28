050ba9ce76c427b663cf79e20cfb840728ad1d28
Ok0lo <78851469+Ok0lo@users.noreply.github.com>
2021-09-28 22:09:39 +0800

    Create Dotting.cs

---
+++ Utils/Dotting.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// is this a clear name?
+public static class Dotting {
+
+    // its good?
+    public static bool IsClosestPositionTo(List<Vector3> candidatePositionList, Vector3 verifiablePosition, Vector3 targetPosition) {
+        float closestDistance = float.MaxValue;
+        foreach(Vector3 candidate in candidatePositionList) {
+            float distance = Vector3.Distance(targetPosition, candidate);
+            if (distance < closestDistance) closestDistance = distance;
+        }
+
+        if (closestDistance > Vector3.Distance(targetPosition, verifiablePosition))
+            return true;
+
+        return false;
+    }
+}
